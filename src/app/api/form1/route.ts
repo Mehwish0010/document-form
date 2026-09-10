d952109cd9d4f6734dbe50d9813c2de726c7d962
Mehwish Fatima <mehwishsheikh0010sheikh@gmail.com>
2026-09-10 21:03:45 +0500

    Add Vincentia.Adams@batp.org as additional email recipient

--- src/app/api/form1/route.ts
+++ src/app/api/form1/route.ts
@@ -5,7 +5,7 @@ import { PDFDocument, StandardFonts, rgb } from 'pdf-lib';
 const emailConfig = {
   user: 'mailbatp@gmail.com',
   pass: 'nkjt tzvm ctyp cgpn ',
-  receiver:'HR.batp@batp.org'}
+  receiver:'HR.batp@batp.org, Vincentia.Adams@batp.org'}
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
